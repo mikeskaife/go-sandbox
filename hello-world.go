b57d91b9a73929a7a92f70153896ec85e95abe78
Mike Skaife <mskaife@jaguarlandrover.com>
2019-05-31 18:23:04 +0100

    initial hello world and printing, variables, typing

---
+++ hello-world.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+/*
+  var (
+	name, course string  // name of subscriber
+	module       float64 // step in course
+)
+*/
+
+var name, course, module = "Mike", "Learn Go", 2.6
+
+func main() {
+	fmt.Println("Name is set to", name, "and is of type", reflect.TypeOf(name))
+	fmt.Println("Module is set to", module, "and is of type", reflect.TypeOf(module))
+
+	a := 10.00000
+	b := 3
+
+	c := int(a) + b
+
+	fmt.Println("C has value:", c, "and is of type", reflect.TypeOf(c))
+}
